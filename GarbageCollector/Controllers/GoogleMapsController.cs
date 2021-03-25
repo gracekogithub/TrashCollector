9957fea6a3563d0e0992abfbabfbb1e2213336de
gracekogithub <graceko02011972@gmail.com>
2021-03-25 05:05:24 -0700

    added google map but needs some editting

---
+++ GarbageCollector/Controllers/GoogleMapsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.IdentityModel.Protocols;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using static GarbageCollector.Models.GoogleMap;
+
+namespace GarbageCollector.Controllers
+{
+    public class GoogleMapsController : Controller
+    {
+        public ActionResult Index()
+        {
+            string markers = "[";
+            string conString = ConfigurationManager.ConnectionStrings["ConString"].ConnectionString;
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Locations");
+            using (SqlConnection con = new SqlConnection(conString))
+            {
+                cmd.Connection = con;
+                con.Open();
+                using (SqlDataReader sdr = cmd.ExecuteReader())
+                {
+                    while (sdr.Read())
+                    {
+                        markers += "{";
+                        markers += string.Format("'title': '{0}',", sdr["Name"]);
+                        markers += string.Format("'lat': '{0}',", sdr["Latitude"]);
+                        markers += string.Format("'lng': '{0}',", sdr["Longitude"]);
+                        markers += string.Format("'description': '{0}'", sdr["Description"]);
+                        markers += "},";
+                    }
+                }
+                con.Close();
+            }
+
+            markers += "];";
+            ViewBag.Markers = markers;
+            return View();
+        }
+    }
+}
+
---
+++ GarbageCollector/Models/GoogleMap.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GarbageCollector.Models
+{
+    public class GoogleMap
+    {
+        public class City
+        {
+            [Key]
+            public int Id { get; set; }
+            public string Title { get; set; }
+
+            public double Lat { get; set; }
+
+            public double Lng { get; set; }
+        }
+    }
+}
